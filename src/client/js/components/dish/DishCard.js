8896ec03ab7d644cb78930a1fdbd05a11ef8c00b
Markus Friesen <Markus.friesen21@gmail.com>
2017-09-22 18:27:46 +0200

    Update menu on DishCard

--- src/client/js/components/dish/DishCard.js
+++ src/client/js/components/dish/DishCard.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Card, CardTitle, CardText, ListItem, ListItemContent, List, ListItemAction, IconButton, Menu, MenuItem } from 'react-mdl';
+import { Card, CardTitle, CardText, ListItem, ListItemContent, List, ListItemAction, IconButton } from 'react-mdl';
 import { Link } from "react-router-dom"
 import { inject } from "mobx-react"
+import { Menu, MenuItem } from 'react-mdl-extra'
 
 @inject('dishStore')
 export default class DishCard extends React.Component {
@@ -13,13 +14,12 @@ export default class DishCard extends React.Component {
     const remove = this.remove.bind(this)
 
     const dishes = this.props.dishGroup.map((d, i) => 
-      <ListItem key={i} threeLine>
+      <ListItem key={i} threeLine >
         <ListItemContent icon="label" subtitle={ d.description }>{ d.name } <span class="float-right">{d.cost.toFixed(2)}</span></ListItemContent>
-        <ListItemAction>
-          <IconButton name="more_vert" id={`menu-${d._id}`} />
-          <Menu target={`menu-${d._id}`} valign="top" align="right" ripple>
-              <Link to={`/dishDetails/${d._id}`}><MenuItem>Edit</MenuItem></Link>
-              <MenuItem onClick={remove(d._id)}>Delete</MenuItem>
+        <ListItemAction >
+          <Menu target={<IconButton name="more_vert" />} align="br tr" ripple>
+            <Link to={`/dishDetails/${d._id}`}><MenuItem>Edit</MenuItem></Link>
+            <MenuItem onClick={remove(d._id)}>Delete</MenuItem>
           </Menu>
         </ListItemAction>
       </ListItem>
