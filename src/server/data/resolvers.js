e1d62b7a0a8a29c5547b14632cbda7a4664adc53
Markus Friesen <markus.friesen@mail.utoronto.ca>
2018-09-30 19:06:16 +0200

    Refactor resolver functions

--- src/server/data/resolvers.js
+++ src/server/data/resolvers.js
@@ -1,77 +1,98 @@
-import { Order, Dish, DishType } from "./Models"
+import {
+  Order,
+  Dish,
+  DishType
+} from "./Models"
 
 const resolvers = {
   Query: {
     orders(_, args) {
       return Order.find(args)
     },
-    dishes(_, args){
+    dishes(_, args) {
       return Dish.find(args)
     },
-    dishTypes(_, args){
+    dishTypes(_, args) {
       return DishType.find(args)
     }
   },
   Mutation: {
-    addOrder(_, args){
+    addOrder(_, args) {
       return Order.create(args)
     },
-    addDish(_, args){
+    addDish(_, args) {
       return Dish.create(args)
     },
-    updateDish(_, args){  
+    updateDish(_, args) {
       const id = args._id
       delete args._id
       return new Promise((resolve, reject) => {
-        Dish.update({ _id: id }, args, (e, r) => {
-          if (e)
-            reject(e)
-          Dish.findOne({_id: id}, (e, r) => {
-            if (e)
-              reject(e)
-            resolve(r)
-          })
-        })
+        Dish.update({
+          _id: id
+        }, args).then(r => {
+          Dish.findOne({
+            _id: id
+          }).then(resolve).catch(reject)
+        }).catch(reject)
       })
     },
-    removeDish(_, args){
+    removeDish(_, args) {
       return new Promise((res, rej) => {
-        Order.find({"dishes.id": args._id}).then(result => {
-            result.forEach(o => {
-              Order.update({ _id: o._id }, { $set : { dishes: o.dishes.filter(d => d.id != args._id) } }).then(console.info).catch(console.error)
-            })
+        Order.find({
+          "dishes.id": args._id
+        }).then(result => {
+          result.forEach(o => {
+            Order.update({
+                _id: o._id
+              }, {
+                $set: {
+                  dishes: o.dishes.filter(d => d.id != args._id)
+                }
+              })
+              .then(console.info)
+              .catch(console.error)
+          })
 
-          Dish.findOneAndRemove({_id: args._id}).then(res).catch(rej)          
+          Dish.findOneAndRemove({
+            _id: args._id
+          }).then(res).catch(rej)
         })
       })
     },
-    addDishType(_, args){
+    addDishType(_, args) {
       return DishType.create(args)
     },
     updateDishType(_, args) {
       const id = args._id
       delete args._id
       return new Promise((resolve, reject) => {
-        DishType.update({ _id: id }, { name: args.name }, (e, r) => {
-          if (e)
-            reject(e)
-          DishType.findOne({_id: id}, (e, r) => {
-            if (e)
-              reject(e)
-            resolve(r)
+        DishType.update({
+            _id: id
+          }, {
+            name: args.name
           })
-        })
+          .then(r => {
+            DishType.findOne({
+                _id: id
+              })
+              .then(resolve)
+              .catch(reject)
+          }).catch(reject)
       })
     },
     removeDishType(_, args) {
       return new Promise((res, rej) => {
-        Dish.find({type: args._id}).then( r => {
+        Dish.find({
+          type: args._id
+        }).then(r => {
           if (r.length > 0) {
             rej("Some dishes with this dish type still exist. Remove those dishes first, before removing the dish type.")
             return
           }
 
-          DishType.findOneAndRemove({ _id: args._id }).then(res).catch(rej)
+          DishType.findOneAndRemove({
+            _id: args._id
+          }).then(res).catch(rej)
         }).catch(rej)
       })
     },
@@ -79,20 +100,19 @@ const resolvers = {
       const id = args._id
       delete args._id
       return new Promise((resolve, reject) => {
-        if (args.dishes){
+        if (args.dishes) {
           args.hasPayed = args.dishes.length === 0 ? (!args.name && !args.table && !args.notes ? true : false) : args.dishes.every(d => d.hasPayed)
         }
 
-        Order.update({ _id: id }, args, (err, result) => {
-          if (err)
-            reject(err)
-
-          Order.findOne({_id: id}, (e, r) => {
-            if (e) //TODO: Rollback here!!
-              reject(e)
-            resolve(r)
-          })
-        })
+        Order.update({
+          _id: id
+        }, args).then(r => {
+          Order.findOne({
+              _id: id
+            })
+            .then(resolve)
+            .catch(reject) //TODO: Rollback here!!
+        }).catch(reject)
       })
     },
     joinOrders(_, args) {
@@ -100,45 +120,46 @@ const resolvers = {
       delete args._id
       const allIds = args.orderIds.concat([id]);
       return new Promise((resolve, reject) => {
-        Order.find({_id: {$in: allIds}}, (e, r) => {
-          if (e)
-            reject(e)
-          else 
-            Order.update({
-                  _id: id
-                }, {
-                  dishes: [].concat.apply([], r.map(m => m.dishes))
-                }, ((e, r) => {
-              if (e)
-                reject(e)
-              else
-                Order.remove({_id: {$in: args.orderIds} }, (e, r) => {
-                  if (e)
-                    reject(e)
-                  else
-                    Order.findOne({_id: id}, (e, r) => {
-                      if (e) //TODO: Rollback here!!
-                        reject(e)
-                      resolve(r)
-                    })
-                })
-            }))
-        })
+        Order.find({
+          _id: {
+            $in: allIds
+          }
+        }).then(r => {
+          Order.update({
+            _id: id
+          }, {
+            dishes: [].concat.apply([], r.map(m => m.dishes))
+          }).then(r => {
+            Order.remove({
+              _id: {
+                $in: args.orderIds
+              }
+            }).then(r => {
+              Order.findOne({
+                _id: id
+              }).then(resolve).catch(reject) //TODO: Rollback here!!
+            }).catch(reject)
+          }).catch(reject)
+        }).catch(reject)
       })
     }
   },
   Dish: {
     type(dish) {
-      return DishType.findOne({ _id: dish.type })
+      return DishType.findOne({
+        _id: dish.type
+      })
     },
   },
   OrderDish: {
     dish(orderDish) {
-      return Dish.findOne({ _id: orderDish.id })
+      return Dish.findOne({
+        _id: orderDish.id
+      })
     },
   },
   Order: {
-    dishes(order){
+    dishes(order) {
       return order.dishes
     }
   }
