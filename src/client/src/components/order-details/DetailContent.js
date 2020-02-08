618da4abe3f0d059eadb5cf0a7269ae0d18e34de
Markus Friesen <markus_friesen@hotmail.com>
2020-02-08 20:09:32 +0100

    Add change to list

--- src/client/src/components/order-details/DetailContent.js
+++ src/client/src/components/order-details/DetailContent.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react'
-import { Grid, GridCell } from '@rmwc/grid';
-import { TabBar, Tab } from '@rmwc/tabs';
-import { Chip, ChipSet } from '@rmwc/chip';
+import React, {Component} from 'react'
+import {Grid, GridCell} from '@rmwc/grid';
+import {TabBar, Tab} from '@rmwc/tabs';
+import {Chip, ChipSet} from '@rmwc/chip';
 import {
   List,
   SimpleListItem
 } from '@rmwc/list';
-import { TextField } from '@rmwc/textfield';
-import { Button } from '@rmwc/button';
+import {TextField} from '@rmwc/textfield';
+import {Button} from '@rmwc/button';
 import gql from "graphql-tag";
-import { Mutation } from "react-apollo";
-import { LinearProgress } from '@rmwc/linear-progress';
+import {Mutation} from "react-apollo";
+import {LinearProgress} from '@rmwc/linear-progress';
 
 import DetailExtraDialog from './DetailExtraDialog';
 
@@ -39,7 +39,7 @@ const UpdateOrder = gql`
 `;
 
 export default class DetailContent extends Component {
-  constructor(){
+  constructor() {
     super()
 
     this.addDish = this.addDish.bind(this)
@@ -53,28 +53,28 @@ export default class DetailContent extends Component {
   state = {
     id: "",
     name: "",
-    table: 0, 
-    notes : "",
+    table: 0,
+    notes: "",
     dishes: [],
     activeTabIndex: 0,
     dishTypes: {},
     extras: {},
     selectedChip: {}
-  } 
+  }
 
-  componentDidMount(){
+  componentDidMount() {
     if (this.props.order)
       this.setState({
         id: this.props.order._id,
         name: this.props.order.name,
         table: this.props.order.table,
         notes: this.props.order.notes,
-        dishes: this.props.order.dishes.map(d => {return {dish: {id: d.dish._id, name: d.dish.name, typeName: d.dish.type.name }, made: d.made, hasPayed: d.hasPayed, extras: d.extras, delivered: d.delivered}})
+        dishes: this.props.order.dishes.map(d => {return {dish: {id: d.dish._id, name: d.dish.name, typeName: d.dish.type.name}, made: d.made, hasPayed: d.hasPayed, extras: d.extras, delivered: d.delivered}})
       })
-      
+
     const dishTypes = this.state.dishTypes
     this.props.dishes.forEach(d => {
-      if (dishTypes[d.type.name]){
+      if (dishTypes[d.type.name]) {
         dishTypes[d.type.name].push(d)
       } else {
         dishTypes[d.type.name] = [d]
@@ -96,15 +96,15 @@ export default class DetailContent extends Component {
     })
   }
 
-  addDish(id, name, typeName){
-    const dishes = [{ dish: { id, name, typeName }, made: false, hasPayed: false }].concat(this.state.dishes)
+  addDish(id, name, typeName) {
+    const dishes = [{dish: {id, name, typeName}, made: false, hasPayed: false}].concat(this.state.dishes)
     this.setState(
       {
         dishes: dishes
       })
   }
 
-  removeDish(){
+  removeDish() {
     const id = this.state.selectedDishId
     let dishes = this.state.dishes
 
@@ -113,21 +113,21 @@ export default class DetailContent extends Component {
 
     if (dishes.length === 1)
       dishes = []
-    else 
+    else
       dishes.splice(id, 1)
-    this.setState({ dishes: dishes})
+    this.setState({dishes: dishes})
   }
 
-  changeTextField(field){
-    return (e) =>{
+  changeTextField(field) {
+    return (e) => {
       const state = {}
       state[field] = e.target.value
       this.setState(state)
     }
   }
 
-  selectDish(id){
-    const { dishes } = this.state
+  selectDish(id) {
+    const {dishes} = this.state
 
     if (!dishes || dishes.length < id) return;
     const dish = dishes[id].dish;
@@ -143,7 +143,7 @@ export default class DetailContent extends Component {
     })
   }
 
-  saveExtras(extras){
+  saveExtras(extras) {
     const dishes = this.state.dishes;
     const dish = dishes[this.state.selectedDishId];
 
@@ -155,38 +155,44 @@ export default class DetailContent extends Component {
   }
 
   render() {
-    const keys = Object.keys(this.state.dishTypes).sort((a, b) => a < b ? -1: 1)
-    const { state, props, selectDish } = this
+    const keys = Object.keys(this.state.dishTypes).sort((a, b) => a < b ? -1 : 1)
+    const {state, props, selectDish} = this
 
     return (
       <React.Fragment>
-      <Grid className="order-details">
-        <GridCell span="12">
-          { keys.length === 0 ? String() : 
+        <Grid className="order-details">
+          <GridCell span="12">
+            {keys.length === 0 ? String() :
               <TabBar
                 activeTabIndex={state.activeTabIndex}
                 onActivate={evt => {
-                  this.setState({ 'activeTabIndex': evt.detail.index })
+                  this.setState({'activeTabIndex': evt.detail.index})
                 }}
               >
                 {keys.map((v, i) => <Tab key={i}>{v}</Tab>)}
               </TabBar>
-          }
-            <ChipSet choice>
+            }
+            <List>
               {
-                (state.dishTypes[keys[state.activeTabIndex]] || []).sort((a, b) => a.name < b.name ? -1 : 1).map((v, i) =>
-                <Chip key={i} onClick={_ => this.addDish(v._id, v.name, keys[state.activeTabIndex])} label={v.name} checkmark selected={
-                  state.dishes.some(d => d.dish.id === v._id)
-                }/>
-              )}
-            </ChipSet>  
+                (state.dishTypes[keys[state.activeTabIndex]] || []).sort((a, b) => a.name < b.name ? -1 : 1).map((v, i) => {
+                  var occur = state.dishes.reduce((c, d) => c + (d.dish.id === v._id ? 1 : 0), 0)
+                  return (
+                    <SimpleListItem
+                      key={i}
+                      onClick={_ => this.addDish(v._id, v.name, keys[state.activeTabIndex])}
+                      text={v.name}
+                      graphic={occur ? "check" : "none "}
+                      meta={occur ? occur : null} />)
+                }
+                )}
+            </List>
 
-        </GridCell>
-      </Grid>
-      <Grid className="order-details">
-        <GridCell span="6">
-          <List className="extendSecondaryText">
-            {state.dishes.map((v, i) => 
+          </GridCell>
+        </Grid>
+        <Grid className="order-details">
+          <GridCell span="6">
+            <List className="extendSecondaryText">
+              {state.dishes.map((v, i) =>
                 <SimpleListItem
                   onClick={() => {
                     if (v.hasPayed || v.made || v.delivered) return;
@@ -199,43 +205,43 @@ export default class DetailContent extends Component {
                   ripple={false}
                   activated={false}
                   disabled={v.hasPayed || v.made || v.delivered}
-                />  
-            )}
-          </List>
-        </GridCell>
-        <GridCell span="6">
+                />
+              )}
+            </List>
+          </GridCell>
+          <GridCell span="6">
             <TextField icon="event_seat" label="Table" type="number" min="0" inputMode="numeric" pattern="\d*" value={state.table} onChange={this.changeTextField("table")} />
             <TextField icon="account_circle" label="Name" value={state.name} onChange={this.changeTextField("name")} />
             <TextField textarea fullwidth label="Notes" type="number" value={state.notes} onChange={this.changeTextField("notes")} />
-        </GridCell>
-      </Grid>
-      <Mutation mutation={props.id ? UpdateOrder : AddOrder}>
-      {(addOrUpdate, {data, loading, error}) => {
-          let result = <Button onClick={() => addOrUpdate({
-            variables: {
-              id: state.id,
-              table: state.table,
-              name: state.name,
-              notes: state.notes,
-              dishes: state.dishes.map(d => { return { id: d.dish.id, made: d.made, hasPayed: d.hasPayed, delivered: d.delivered, extras: (d.extras|| []).map(e => e._id)} }).filter(d => d && d.id)
-            }
-          })} theme="secondary">Save</Button>
+          </GridCell>
+        </Grid>
+        <Mutation mutation={props.id ? UpdateOrder : AddOrder}>
+          {(addOrUpdate, {data, loading, error}) => {
+            let result = <Button onClick={() => addOrUpdate({
+              variables: {
+                id: state.id,
+                table: state.table,
+                name: state.name,
+                notes: state.notes,
+                dishes: state.dishes.map(d => {return {id: d.dish.id, made: d.made, hasPayed: d.hasPayed, delivered: d.delivered, extras: (d.extras || []).map(e => e._id)}}).filter(d => d && d.id)
+              }
+            })} theme="secondary">Save</Button>
 
-          if (loading) 
-            result = 
-            <React.Fragment>
-              <LinearProgress />
-              {result}
-            </React.Fragment>
-          if (error) console.error(error);
+            if (loading)
+              result =
+                <React.Fragment>
+                  <LinearProgress />
+                  {result}
+                </React.Fragment>
+            if (error) console.error(error);
 
-          if (data) props.history.goBack()
+            if (data) props.history.goBack()
 
-          return result
-        }}
+            return result
+          }}
         </Mutation>
-        <DetailExtraDialog 
-          open={this.state.openDialog} 
+        <DetailExtraDialog
+          open={this.state.openDialog}
           onClose={() => this.setState({openDialog: false})}
           dish={this.state.selectedDish}
           onDelete={this.removeDish}
