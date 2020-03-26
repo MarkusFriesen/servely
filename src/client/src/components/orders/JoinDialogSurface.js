4f79032cacdda0d07562d4e32671f5a4144f3858
Markus Friesen <markus_friesen@hotmail.com>
2020-03-26 14:19:59 +0100

    Make JoinOrderDialog a functional component

--- src/client/src/components/orders/JoinDialogSurface.js
+++ src/client/src/components/orders/JoinDialogSurface.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, {useState, useEffect} from 'react'
 import {
   DialogTitle,
   DialogContent,
@@ -12,7 +12,7 @@ import {
   ListItemGraphic,
 } from '@rmwc/list';
 import gql from "graphql-tag";
-import { Mutation } from "react-apollo";
+import {Mutation} from "react-apollo";
 
 const JOIN = gql`
   mutation join($id: ID!, $ids: [ID]!){
@@ -36,73 +36,51 @@ const JOIN = gql`
     }
   }`
 
-export default class JoinOrderSurface extends Component {
-  constructor(){
-    super()
+function handleJoin(i, people, setPeople) {
+  const selected = people.map((p, idx) => idx === i ? {...p, selected: !p.selected} : p)
+  setPeople(selected)
+}
 
-    this.handleJoin = this.handleJoin.bind(this)
-    this.joinOrders = this.joinOrders.bind(this)
+function joinOrders(join, people, id) {
+  return () => {
+    const ordersToJoin = people.filter(s => s.selected).map(s => s.id)
+    join({variables: {id: id, ids: ordersToJoin}})
   }
-  
-  state = {
-    people: []
-  }
+}
+const JoinOrderSurface = (props) => {
 
-  componentWillReceiveProps(newProps, oldProps){
-    if (JSON.stringify(newProps) !== JSON.stringify(oldProps))
-      this.setState({
-        people: newProps.data.map((o, i) => { return { id: o._id, name: o.name, selected: (this.state.people.length > i ? this.state.people[i].selected : false) } })
-      })
-  }
+  const [people, setPeople] = useState(props.data.map(o => ({id: o._id, name: o.name, selected: false})))
 
-  componentDidMount(){
-    this.setState({
-      people: this.props.data.map(o => {return {id: o._id, name: o.name, selected: false}})
-    })
-  }
-
-  handleJoin(i){
-    return () => {
-      const selected = this.state.people
-      selected[i].selected = !selected[i].selected
-      this.setState({
-        people: selected
-      })
-      
+  useEffect(() => {
+    if (props.data.length !== people.length || props.data.some((p, idx) => p._id !== people[idx].id)) {
+      setPeople(props.data.map((o, i) => {return {id: o._id, name: o.name, selected: (people.length > i ? people[i].selected : false)}}))
     }
-  }
+  }, [props.data, people.length, people])
 
-  joinOrders(join){
-    return () => {
-      const ordersToJoin = this.state.people.filter(s => s.selected).map(s => s.id)
-      join({variables: {id: this.props._id, ids: ordersToJoin}})
-    }
-  }
-
-  render(){
-    return (
-      <React.Fragment>
-        <DialogTitle>Everyone at table {this.props.table}</DialogTitle>
-        < DialogContent >
-          <List>
+  return (
+    <React.Fragment>
+      <DialogTitle>Everyone at table {props.table}</DialogTitle>
+      <DialogContent>
+        <List>
           {
-            this.state.people.map((p, i) => 
-                <ListItem key={p.id} onClick={this.handleJoin(i)}>
-                <ListItemGraphic icon={p.selected ? "radio_button_checked" : "radio_button_unchecked"}/>
-                <ListItemText>{ p.name }</ListItemText>
+            people.map((p, i) =>
+              <ListItem key={p.id} onClick={() => handleJoin(i, people, setPeople)}>
+                <ListItemGraphic icon={p.selected ? "radio_button_checked" : "radio_button_unchecked"} />
+                <ListItemText>{p.name}</ListItemText>
               </ListItem>
             )
           }
-          </List>
-        </DialogContent>
-        <DialogActions>
-          <DialogButton action="close">Cancel</DialogButton >
-          <Mutation mutation={JOIN}>
-            {(join) => 
-              <DialogButton action="accept" onClick={this.joinOrders(join)} isDefaultAction>Join</DialogButton >
-            }
-            </Mutation>
-        </DialogActions>
-      </React.Fragment>)
-  }
+        </List>
+      </DialogContent>
+      <DialogActions>
+        <DialogButton action="close">Cancel</DialogButton >
+        <Mutation mutation={JOIN}>
+          {(join) =>
+            <DialogButton action="accept" onClick={joinOrders(join, people, props.id)} isDefaultAction>Join</DialogButton >
+          }
+        </Mutation>
+      </DialogActions>
+    </React.Fragment>)
 }
+
+export default JoinOrderSurface
