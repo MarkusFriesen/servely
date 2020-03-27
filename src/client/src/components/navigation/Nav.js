dab010c725e6b6f7b9cef806e554c60148466a0d
Markus Friesen <markus_friesen@hotmail.com>
2020-03-27 16:22:38 +0100

    Make Nav a functional component

--- src/client/src/components/navigation/Nav.js
+++ src/client/src/components/navigation/Nav.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import { inject, observer } from 'mobx-react'
+import React, {useState} from 'react'
+import {inject, observer} from 'mobx-react'
 import {
   TopAppBar,
   TopAppBarRow,
@@ -13,126 +13,108 @@ import {
   ListItemGraphic,
   ListItem,
   ListItemText
-} from '@rmwc/list';
+} from '@rmwc/list'
 import {
   Drawer,
   DrawerHeader,
   DrawerContent,
   DrawerTitle
-} from '@rmwc/drawer';
+} from '@rmwc/drawer'
 import {
   Link
 } from 'react-router-dom'
-
 import './Nav.css'
 
-class Nav extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      searching: false,
-      searchText: "",
-      modalOpen: false
-    }
+const handleSearchText = (props, setSearchText) => (e) => {
+  setSearchText(e.target.value)
+  props.store.searchText = e.target.value
+}
 
-    this.toggleSearch = this.toggleSearch.bind(this)
-    this.handleSearchText = this.handleSearchText.bind(this)
-    this.onClick = this.onClick.bind(this)
-  }
+function toggleSearch(props, setSearchText, searching, setSearching) {
+  setSearching(!searching)
+  setSearchText('')
+  props.store.searchText = ''
+}
 
-  
-  onClick(){
-    this.setState({ modalOpen: !this.state.modalOpen })
-  }
-  handleSearchText(){
-    return (e)=>{
-      this.setState({
-        searchText: e.target.value
-      })
-      this.props.store.searchText = e.target.value
-    }
-  }
+const closeModal = (setIsModalOpen) => () => {
+  setIsModalOpen(false)
+}
+const Nav = (props) => {
 
-  toggleSearch(){
-    this.setState({ searching: !this.state.searching, searchText: "" })
-    this.props.store.searchText = ""
-  }
-
-  render(){
-    const { toggleSearch, onClick } = this 
-    if (this.state.searching)
-      return (
-        <TopAppBar dense fixed>
-          <TopAppBarRow>
-            <TopAppBarSection className="search-field" alignStart>
-              <input placeholder="Search" autoFocus value={this.state.searchText} onChange={this.handleSearchText()}/>
-              <TopAppBarActionItem aria-label="Search" icon="clear" alt="Search this Page" onClick={toggleSearch}/>
-            </TopAppBarSection>          
-          </TopAppBarRow>
-       </TopAppBar>
-      )
-
+  const [searching, setSearching] = useState(false)
+  const [searchText, setSearchText] = useState('')
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  if (searching)
     return (
-      <React.Fragment>
-        <TopAppBar dense fixed className="no-print">
-          <TopAppBarRow>
-            <TopAppBarSection alignStart>
-              <TopAppBarNavigationIcon icon="menu" onClick={onClick}/>
-              <TopAppBarTitle>{this.props.store.kitchenMode ? "BIt's Kitchen" : "BIt"}</TopAppBarTitle>
-            </TopAppBarSection>
-            <TopAppBarSection alignEnd>
-              <TopAppBarActionItem aria-label="Search" icon="search" alt="Search this Page" onClick={toggleSearch}/>
-            </TopAppBarSection>
-          </TopAppBarRow>
-        </TopAppBar>
+      <TopAppBar dense fixed>
+        <TopAppBarRow>
+          <TopAppBarSection className="search-field" alignStart>
+            <input placeholder="Search" autoFocus value={searchText} onChange={handleSearchText(props, setSearchText)} />
+            <TopAppBarActionItem aria-label="Search" icon="clear" alt="Search this Page" onClick={() => toggleSearch(props, setSearchText, searching, setSearching)} />
+          </TopAppBarSection>
+        </TopAppBarRow>
+      </TopAppBar>
+    )
 
-        <Drawer
-            modal
-            open={this.state.modalOpen}
-            onClose={() => this.setState({ modalOpen: false })}
-          >
-            <DrawerHeader>
-              <DrawerTitle>Menu</DrawerTitle>
-            </DrawerHeader>
-            <DrawerContent>
-              <List>
-                <Link to="/orders">
-                  <ListItem onClick={onClick}>
-                    <ListItemGraphic icon="shopping_cart" />
-                    <ListItemText>Orders</ListItemText>
-                  </ListItem>
-                </Link>
-                <Link to="/orderHistory">
-                <ListItem onClick={onClick}>
-                  <ListItemGraphic icon="history"/>
-                  <ListItemText>History</ListItemText>
-                  </ListItem>
-                </Link>
-                <Link to="/dishes">
-                <ListItem onClick={onClick}>
-                  <ListItemGraphic icon = "shopping_basket" />
-                  <ListItemText>Dishes</ListItemText>
-                  </ListItem>
-                </Link>
-                <Link to="/dishExtra">
-                <ListItem onClick={onClick}>
-                  <ListItemGraphic icon = "loyalty" />
-                  <ListItemText>Extras</ListItemText>
-                  </ListItem>
-                </Link>
-                <Link to="/settings">
-                <ListItem onClick={onClick}>
-                  <ListItemGraphic icon="settings" />  
-                  <ListItemText>Settings</ListItemText>
-                  </ListItem>
-                </Link>
-              </List>
-            </DrawerContent>
-          </Drawer>
-      </React.Fragment>
+  return (
+    <React.Fragment>
+      <TopAppBar dense fixed className="no-print">
+        <TopAppBarRow>
+          <TopAppBarSection alignStart>
+            <TopAppBarNavigationIcon icon="menu" onClick={() => setIsModalOpen(true)} />
+            <TopAppBarTitle>{props.store.kitchenMode ? "BIt's Kitchen" : "BIt"}</TopAppBarTitle>
+          </TopAppBarSection>
+          <TopAppBarSection alignEnd>
+            <TopAppBarActionItem aria-label="Search" icon="search" alt="Search this Page" onClick={() => toggleSearch(props, setSearchText, searching, setSearching)} />
+          </TopAppBarSection>
+        </TopAppBarRow>
+      </TopAppBar>
 
-    );
-  }
+      <Drawer
+        modal
+        open={isModalOpen}
+        onClose={closeModal(setIsModalOpen)}
+      >
+        <DrawerHeader>
+          <DrawerTitle>Menu</DrawerTitle>
+        </DrawerHeader>
+        <DrawerContent>
+          <List>
+            <Link to="/orders">
+              <ListItem onClick={closeModal(setIsModalOpen)}>
+                <ListItemGraphic icon="shopping_cart" />
+                <ListItemText>Orders</ListItemText>
+              </ListItem>
+            </Link>
+            <Link to="/orderHistory">
+              <ListItem onClick={closeModal(setIsModalOpen)}>
+                <ListItemGraphic icon="history" />
+                <ListItemText>History</ListItemText>
+              </ListItem>
+            </Link>
+            <Link to="/dishes">
+              <ListItem onClick={closeModal(setIsModalOpen)}>
+                <ListItemGraphic icon="shopping_basket" />
+                <ListItemText>Dishes</ListItemText>
+              </ListItem>
+            </Link>
+            <Link to="/dishExtra">
+              <ListItem onClick={closeModal(setIsModalOpen)}>
+                <ListItemGraphic icon="loyalty" />
+                <ListItemText>Extras</ListItemText>
+              </ListItem>
+            </Link>
+            <Link to="/settings">
+              <ListItem onClick={closeModal(setIsModalOpen)}>
+                <ListItemGraphic icon="settings" />
+                <ListItemText>Settings</ListItemText>
+              </ListItem>
+            </Link>
+          </List>
+        </DrawerContent>
+      </Drawer>
+    </React.Fragment>
+  )
 }
 
 export default inject("store")(observer(Nav))
